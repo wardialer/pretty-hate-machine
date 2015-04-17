a8808b976a0f47d771146475447b5d131350d349
giravedu <giuseppe.raveduto@eng.it>
2015-04-17 23:33:10 +0200

    added tasks for create dist package and run in develop mode

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,9 +19,24 @@ module.exports = function(grunt) {
       },
       build: {
         files: {
-          'public/dist/js/app.min.js': 'public/js/**/*.js'
+          'dist/js/app.min.js': 'public/js/**/*.js'
         }
       }
+    },
+    //preprocess sass files
+    sass: {
+        options: {
+            sourceMap: true
+        },
+        dist: {
+            files: [{
+                expand: true,
+                cwd: 'public/css/sass/',
+                src: ['*.scss'],
+                dest: 'public/css',
+                ext: '.css'
+            }]
+        }
     },
     //minify css files
     cssmin: {
@@ -30,31 +45,69 @@ module.exports = function(grunt) {
       },
       build: {
         files: {
-          'public/dist/css/style.min.css': 'public/css/style.css'
+          'dist/css/style.min.css': 'public/css/**/*.css'
         }
       }
     },
-    //auto update
-    watch: {
-        stylesheets: { 
-            files: ['**/*.css', '**/*.less', '!**/dist/**/*.css', '!**/dist/**/*.less'], 
-            tasks: ['cssmin'] 
+    //preprocess index.html
+    targethtml: {
+        dist: {
+            files: {
+                'public/views/index.html': 'public/views/index.src.html'
+            }
         },
-        scripts: { 
-            files: ['**/*.js', '!**/dist/**/*.js'], 
-            tasks: ['jshint', 'uglify'] 
-        } 
+        dev: {
+            files: {
+                'public/views/index.html': 'public/views/index.src.html'
+            }
+        }
+    },
+    //create dist package
+    compress: {
+        main: {
+            options: {
+                archive: 'dist/<%= pkg.name %>V<%= pkg.version %>.tar.gz'
+            },
+            files: [
+                {
+                    expand: true,
+                    cwd: '.',
+                    src: [
+                        'dist/**/*.js',
+                        'dist/**/*.css',
+                        'public/views/**', 
+                        'config/**', 
+                        'app/**',
+                        '.bowerrc',
+                        'bower.json',
+                        'package.json',
+                        'server.js'
+                        ],
+                    dest: '',
+                }
+            ]
+        }
+    },
+    //nodemon
+    nodemon: {
+        dev: {
+            script: './server.js'
+        }
     }
 
 
     });
     
-    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin']);
+    grunt.registerTask('default', ['targethtml:dev', 'nodemon']);
+    grunt.registerTask('build', ['jshint', 'uglify', 'sass', 'cssmin', 'targethtml:dist', 'compress']);
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-targethtml');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-nodemon');
 
 };
