048c3e0d4ee18b93ede2b73fe602e5dd2e782887
giravedu <giuseppe.raveduto@eng.it>
2015-04-20 20:26:01 +0200

    removed unused field

--- app/models/element.js
+++ app/models/element.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 var elementSchema = mongoose.Schema({
     name: {type: String, default: ''},
-    modifiedAt: {type: Date}
 });   
 
 module.exports = mongoose.model('Element', elementSchema);
